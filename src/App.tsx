2074b57b2e3f49abd281c2d7f26f85a81d34440f
Joao-Eduardo-AJ <joaoeduaj@gmail.com>
2023-05-04 22:02:23 -0300

    feat: adds global styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './routes';
 import { ThemeProvider } from '@emotion/react';
 import { getOrCreateTheme } from './styles/theme';
+import './styles/global.css';
 
 function App() {
   return (
