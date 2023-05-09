31cf881b6ca8161d0399e3454c3b0a95b1ec7e17
Joao-Eduardo-AJ <joaoeduaj@gmail.com>
2023-05-08 22:55:18 -0300

    feat: adds register page

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,9 +1,32 @@
-import { Card, Typography } from '@mui/material';
+import { Form } from '@unform/web';
+import { FormLayout } from '../../components/FormLayout';
+import { Button, Grid } from '@mui/material';
+import { VTextField } from '../../components/VTextField';
+import { FormHandles } from '@unform/core';
+import { useRef } from 'react';
+
+interface ISubmitData {
+  email: string;
+  password: string;
+}
 
 export function Register() {
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmitForm = (data: ISubmitData) => console.log(data);
+
   return (
-    <Card>
-      <Typography>register</Typography>
-    </Card>
+    <FormLayout>
+      <Form ref={formRef} onSubmit={handleSubmitForm}>
+        <Grid container gap={1}>
+          <VTextField label="Email" name="email" />
+          <VTextField label="Password" name="password" />
+          <VTextField label="Repeat the password" name="repeat" />
+          <Button variant="contained" type="submit" fullWidth>
+            Register
+          </Button>
+        </Grid>
+      </Form>
+    </FormLayout>
   );
 }
