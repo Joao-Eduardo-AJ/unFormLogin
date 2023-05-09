5ed14ccf421b9de4a0750ac243b5f48c3c7ed183
Joao-Eduardo-AJ <joaoeduaj@gmail.com>
2023-05-08 21:36:19 -0300

    style: create global styles

---
+++ src/styles/styles.ts
@@ -0,0 +1,26 @@
+import { makeStyles } from 'tss-react/mui';
+
+export const useStyles = makeStyles()(() => ({
+  paper: {
+    backgroundColor: '#F8F3FB',
+    height: '100vh',
+    padding: '1rem',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  textField: {
+    backgroundColor: '#FEFAF7',
+    borderRadius: '.6rem',
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        border: 'none',
+      },
+    },
+  },
+  link: {
+    color: 'rgba(0, 0, 0, 0.6)',
+    textDecoration: 'none',
+  },
+}));
