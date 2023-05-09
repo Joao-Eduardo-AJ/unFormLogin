6bc29ced034914222b6886b8b74913d7cf0080c6
Joao-Eduardo-AJ <joaoeduaj@gmail.com>
2023-05-08 22:54:58 -0300

    feat: adds login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,9 +1,31 @@
-import { Card, Typography } from '@mui/material';
+import { Form } from '@unform/web';
+import { FormLayout } from '../../components/FormLayout';
+import { Button, Grid } from '@mui/material';
+import { VTextField } from '../../components/VTextField';
+import { FormHandles } from '@unform/core';
+import { useRef } from 'react';
+
+interface ISubmitData {
+  email: string;
+  password: string;
+}
 
 export function Login() {
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmitForm = (data: ISubmitData) => console.log(data);
+
   return (
-    <Card>
-      <Typography>Login</Typography>
-    </Card>
+    <FormLayout>
+      <Form ref={formRef} onSubmit={handleSubmitForm}>
+        <Grid container gap={2}>
+          <VTextField label="email" name="email" />
+          <VTextField label="password" name="password" />
+          <Button variant="contained" type="submit" fullWidth>
+            Login
+          </Button>
+        </Grid>
+      </Form>
+    </FormLayout>
   );
 }
