85fbdf882dde3cf52afc51013946da0e523bdf71
Joao-Eduardo-AJ <joaoeduaj@gmail.com>
2023-05-09 23:50:23 -0300

    style: update application layout

--- src/components/FormLayout/index.tsx
+++ src/components/FormLayout/index.tsx
@@ -14,52 +14,61 @@ export function FormLayout({ children }: FormLayoutProps) {
 
   return (
     <Card className={classes.paper}>
-      <Grid container>
-        <Grid item alignSelf={['start']}>
+      <Grid container gap={4}>
+        <Grid container item>
+          <Grid item alignSelf={['start']}>
+            <Typography
+              variant="subtitle2"
+              fontWeight={700}
+              display="flex"
+              alignItems="center"
+            >
+              {location === '/' ? 'Login' : 'Sign up'}
+              <Person2Outlined fontSize="small" />
+            </Typography>
+            <Typography variant="body2" color="#00000099" fontSize={12}>
+              {location === '/' ? 'Welcome back!' : 'Welcome!'}
+            </Typography>
+          </Grid>
+          <Grid
+            container
+            item
+            alignItems="center"
+            direction="column"
+            gap={2}
+            marginTop={4}
+          >
+            <img src="/public/images/login.svg" height={220} />
+            <Typography variant="body2" fontWeight={800} color="primary">
+              {location === '/'
+                ? 'Enter your login details ;)'
+                : 'Enter your registration details ;)'}
+            </Typography>
+          </Grid>
+        </Grid>
+        <CardContent sx={{ padding: '0' }}>{children}</CardContent>
+        <Grid container item direction="column" alignItems="center" gap={3}>
           <Typography
-            variant="subtitle2"
-            fontWeight={700}
+            variant="body2"
+            color="#00000099"
+            fontSize={12}
+            display="flex"
+            gap={1}
+          >
+            <img src="/public/images/pipe.svg" />
+            Or {location === '/' ? 'login' : 'sign up'} with
+            <img src="/public/images/pipe.svg" />
+          </Typography>
+          <Typography
             display="flex"
             alignItems="center"
+            gap={0.5}
+            fontWeight={600}
           >
-            {location === '/' ? 'Login' : 'Sign up'}
-            <Person2Outlined fontSize="small" />
-          </Typography>
-          <Typography variant="body2" color="#00000099" fontSize={12}>
-            {location === '/' ? 'Welcome back!' : 'Welcome!'}
+            <img src="/public/images/google.svg" alt="google" />
+            Google
           </Typography>
         </Grid>
-        <Grid container item alignItems="center" direction="column" gap={2}>
-          <img src="/public/images/login.svg" height={190} />
-          <Typography variant="body2" fontWeight={800} color="primary">
-            {location === '/'
-              ? 'Enter your login details ;)'
-              : 'Enter your registration details ;)'}
-          </Typography>
-        </Grid>
-      </Grid>
-      <CardContent sx={{ padding: '0' }}>{children}</CardContent>
-      <Grid container item direction="column" alignItems="center" gap={2}>
-        <Typography
-          variant="body2"
-          color="#00000099"
-          fontSize={12}
-          display="flex"
-          gap={1}
-        >
-          <img src="/public/images/pipe.svg" />
-          Or {location === '/' ? 'login' : 'sign up'} with
-          <img src="/public/images/pipe.svg" />
-        </Typography>
-        <Typography
-          display="flex"
-          alignItems="center"
-          gap={0.5}
-          fontWeight={600}
-        >
-          <img src="/public/images/google.svg" alt="google" />
-          Google
-        </Typography>
       </Grid>
       <Typography color="#00000099" fontSize={12}>
         {location === '/'
