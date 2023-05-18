3145ff54631a7799567cbebcbbfe98b039a33dc5
Joao-Eduardo-AJ <joaoeduaj@gmail.com>
2023-05-18 11:33:56 -0300

    feat: update registered users initial arg

--- src/reducer/initialArg.ts
+++ src/reducer/initialArg.ts
@@ -2,5 +2,31 @@ import { State } from '.';
 
 export const initialState: State = {
   alertSnackBarVisible: false,
-  registeredUsers: [],
+  registeredUsers: [
+    {
+      id: 1,
+      email: 'tucano_brabissimo_no_front@gmail.com',
+      password: '123123',
+    },
+    {
+      id: 2,
+      email: 'shaolin_matador_de_porco@gmail.com',
+      password: '123123',
+    },
+    {
+      id: 3,
+      email: 'flavinho_do_pneu@gmail.com',
+      password: '123123',
+    },
+    {
+      id: 4,
+      email: 'jessikinhaa_mandrak4@gmail.com',
+      password: '123123',
+    },
+    {
+      id: 5,
+      email: 'clonagem_de_cartao_ltda@gmail.com',
+      password: '123123',
+    },
+  ],
 };
