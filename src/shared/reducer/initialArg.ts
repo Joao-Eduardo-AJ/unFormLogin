9c87a359c5a03dc1cc2f8978263aec3300850eb9
Joao-Eduardo-AJ <joaoeduaj@gmail.com>
2023-06-01 22:12:16 -0300

    feat: update reducer initial arguments

--- src/shared/reducer/initialArg.ts
+++ src/shared/reducer/initialArg.ts
@@ -6,28 +6,8 @@ export const initialState: State = {
   registeredUsers: [
     {
       id: 1,
-      email: 'tucano_brabissimo_no_front@gmail.com',
-      password: '123123',
-    },
-    {
-      id: 2,
-      email: 'shaolin_matador_de_porco@gmail.com',
-      password: '123123',
-    },
-    {
-      id: 3,
-      email: 'flavinho_do_pneu@gmail.com',
-      password: '123123',
-    },
-    {
-      id: 4,
-      email: 'jessikinhaa_mandrak4@gmail.com',
-      password: '123123',
-    },
-    {
-      id: 5,
-      email: 'clonagem_de_cartao_ltda@gmail.com',
-      password: '123123',
+      email: 'joaoeduaj@gmail.com',
+      password: '&@OAj1404',
     },
   ],
 };
